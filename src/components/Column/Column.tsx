0f9f2bc727db3d01d2bfdd35c7a9b260b19d82dd
Eduard Cristian Tasca <eduardcristiantasca@gmail.com>
2022-09-26 23:03:12 +0300

    Simply drag n drop

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGrip } from '@fortawesome/free-solid-svg-icons';
+import { useState, useRef } from 'react';
 
 interface Card {
   title: string
@@ -17,16 +18,56 @@ interface Props {
 const Column = ({
   columnData
 }: Props) => {
+  const dragItem = useRef<number>();
+  const dragOverItem = useRef<number>();
+
+  const [ cardList, setCardList ] = useState(columnData.list);
+
+  const dragStart = (e: React.DragEvent<HTMLButtonElement>, position: number) => {
+    dragItem.current = position;
+    e.currentTarget.classList.add("dragging")
+  };
+ 
+  const dragEnter = (e: React.DragEvent<HTMLButtonElement>, position: number) => {
+    dragOverItem.current = position;
+  };
+ 
+  const drag = (e: React.DragEvent<HTMLButtonElement>) => {
+    console.log("dragging")
+  }
+
+  const drop = (e: React.DragEvent<HTMLButtonElement>) => {
+    const tempCardList = [...cardList];
+    let dragIndex = Number(dragItem.current);
+    let newDragIndex = Number(dragOverItem.current);
+
+    const dragItemContent = tempCardList[dragIndex];
+    tempCardList.splice(dragIndex, 1);
+    tempCardList.splice(newDragIndex, 0, dragItemContent);
+    dragIndex = 0;
+    newDragIndex = 0;
+    
+    setCardList(tempCardList);
+    e.currentTarget.classList.remove("dragging")
+  };
+
   const renderList = () => {
     return (
       <ul className="list">
-        {columnData.list.map((card, index) => {
+        {cardList.map((card, index) => {
           return (
             <li 
               key={index}
               className="item"
             >
-              <button className="card-btn drag">
+              <button 
+                className="btn-grip"
+                draggable
+                onDragStart={(e) => dragStart(e, index)}
+                onDragEnter={(e) => dragEnter(e, index)}
+                onDrag={drag}
+                onDragEnd={drop}
+              >
                 <span>{card.title}</span>
                 <FontAwesomeIcon icon={faGrip} />
               </button>
